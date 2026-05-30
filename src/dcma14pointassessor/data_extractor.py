89c955830e4bf276059c18660d74285ab8ae26c2
Chris Allsberry <chrisallsberry@gmail.com>
2026-05-30 21:44:40 +0000

    Completed DataExtractor Module.

---
+++ src/dcma14pointassessor/data_extractor.py
@@ -0,0 +1,197 @@
+
+import os
+from dataclasses import dataclass
+import datetime as dt
+
+import jpype
+import jpype.imports
+import mpxj
+
+from .project_classes import ProjectData, ProjectProperties, Task, TaskRelation, ResourceAssignment
+
+if not jpype.isJVMStarted():
+    jpype.startJVM("-Dlog4j2.loggerContextFactory=org.apache.logging.log4j.simple.SimpleLoggerContextFactory") # Move this to the start of the app
+
+from java.io import ByteArrayInputStream
+from java.lang import Byte
+from org.mpxj.reader import UniversalProjectReader
+
+
+class DataExtractor:
+    def __init__(self, filepath_or_bytes: str | bytes):
+        self.file = self.__bytes_converter(filepath_or_bytes)
+        self.project = UniversalProjectReader().read(self.file)
+
+
+    def __bytes_converter(self, file: str | bytes):
+        if type(file) == str:
+            return file
+        else:
+            java_bytes = jpype.JArray(jpype.JByte)(file)
+            input_stream = ByteArrayInputStream(java_bytes)
+            return input_stream
+
+    def __to_string(self, input) -> str | None:
+        if input == None:
+            return None
+        else:
+            try:
+                return str(input.toString())
+            except:
+                return str(input)
+
+    def __java_to_datetime(self, input) -> dt.datetime | None:
+        if input == None:
+            return None
+        else:
+            date_string = str(input.toString())
+            date = dt.datetime.strptime(date_string, '%Y-%m-%dT%H:%M')
+            return date
+
+    def __str_to_datetime(self, input) -> dt.datetime | None:
+        if input == None:
+            return None
+        else:
+            date = dt.datetime.strptime(input, '%Y-%m-%dT%H:%M')
+            return date
+
+    def __to_datetime_from_iso(self, input):
+        if input == None:
+            return None
+        else:
+            return dt.datetime.fromisoformat(input)
+
+
+    def __to_number(self, input) -> int | float | None:
+        if input == None:
+            return input
+        else:
+            try:
+                return int(input)
+            except:
+                return float(input)
+
+
+    def __get_duration(self, input) -> dict:
+        if input == None:
+            return None
+        else:
+            return {
+                'Value': float(input.getDuration()),
+                'Unit': str(input.getUnits().getName())
+            }
+
+
+    def __extract_project_properties(self) -> ProjectProperties:
+        properties = self.project.getProjectProperties()
+        status_string = self.__to_string(properties.getStatusDate())
+        output = ProjectProperties(
+            status_date=self.__str_to_datetime(status_string)
+        )
+        return output
+
+
+    def __extract_tasks(self) -> list[Task]:
+        output = []
+        for task in self.project.getTasks():
+            data = {
+                "id": self.__to_number(task.getID()),
+                "name": self.__to_string(task.getName()),
+                "baseline_duration": self.__get_duration(task.getBaselineDuration()),
+                "duration": self.__get_duration(task.getDuration()),
+                "actual_duration": self.__get_duration(task.getActualDuration()),
+                "baseline_start": self.__java_to_datetime(task.getBaselineStart()),
+                "start": self.__java_to_datetime(task.getStart()),
+                "actual_start": self.__java_to_datetime(task.getActualStart()),
+                "baseline_finish": self.__java_to_datetime(task.getBaselineFinish()),
+                "finish": self.__java_to_datetime(task.getFinish()),
+                "actual_finish": self.__java_to_datetime(task.getActualFinish()),
+                "baseline_work": self.__get_duration(task.getBaselineWork()),
+                "work": self.__get_duration(task.getWork()),
+                "actual_work": self.__get_duration(task.getActualWork()),
+                "milestone": task.getMilestone(),
+                "summary": task.getSummary(),
+                "critical": task.getCritical(),
+                "guid": self.__to_string(task.getGUID()),
+                "resume": self.__java_to_datetime(task.getResume()), 
+                "stop": self.__java_to_datetime(task.getStop()),
+                'constraint_date' : self.__java_to_datetime(task.getConstraintDate()),
+                'constraint_type' : self.__to_string(task.getConstraintType()),
+                'active': task.getActive(),
+                'task_mode': self.__to_string(task.getTaskMode()),
+                'type': self.__to_string(task.getType()),
+                'wbs': self.__to_string(task.getWBS()),
+                'outline_level' : self.__to_number(task.getOutlineLevel()),
+                'outline_number' : self.__to_string(task.getOutlineNumber()),
+            }
+            new_task = Task(**data)
+            output.append(new_task)
+        return output
+
+
+    def __extract_task_relations(self):
+
+        def get_relation_info(rel) -> TaskRelation:
+            data = {
+                        "lag": self.__get_duration(rel.getLag()),
+                        "notes": self.__to_string(rel.getNotes()),
+                        "predecessor": self.__to_string(rel.getPredecessorTask().getGUID()),
+                        "successor": self.__to_string(rel.getSuccessorTask().getGUID()),
+                        "type": self.__to_string(rel.getType()),
+                        "uid": self.__to_number(self.__to_string(rel.getUniqueID()))
+                    }
+            return TaskRelation(**data)
+
+        output = []
+        for task in self.project.getTasks():
+            for rel in task.getPredecessors():
+                output.append(get_relation_info(rel))
+
+        return output
+
+
+    def __extract_resource_assignments(self) -> list[ResourceAssignment]:
+        output = []
+        for resource in self.project.getResources():
+            for ra in resource.getTaskAssignments():
+                data = { # Find Duration
+                    'actual_finish': self.__java_to_datetime(ra.getActualFinish()),
+                    'actual_start': self.__java_to_datetime(ra.getActualStart()),
+                    'actual_work': self.__get_duration(ra.getActualWork()),
+                    'baseline_finish': self.__java_to_datetime(ra.getBaselineFinish()),
+                    'baseline_start': self.__java_to_datetime(ra.getBaselineStart()),
+                    'baseline_work': self.__get_duration(ra.getBaselineWork()),
+                    'create_date': self.__java_to_datetime(ra.getCreateDate()),
+                    'delay': self.__get_duration(ra.getDelay()),
+                    'finish': self.__java_to_datetime(ra.getFinish()),
+                    'guid': self.__to_string(ra.getGUID()),
+                    'resource': self.__to_string(ra.getResource().getName()),
+                    'start': self.__java_to_datetime(ra.getStart()),
+                    'task_guid': self.__to_string(ra.getTask().getGUID()),
+                    'work': self.__get_duration(ra.getWork()),
+                }
+                new_ra = ResourceAssignment(**data)
+                output.append(new_ra)
+        return output
+
+    def extract_data(self) -> ProjectData:
+        project_properties = self.__extract_project_properties()
+        tasks = self.__extract_tasks()
+        task_relations = self.__extract_task_relations()
+        resource_assignments = self.__extract_resource_assignments()
+        return ProjectData(
+            project_properties=project_properties,
+            tasks=tasks,
+            task_relations=task_relations,
+            resource_assigments=resource_assignments
+        )
+
+
+
+if __name__ == '__main__':
+    path = os.path.join('.', 'TEMP', 'project_files', 'Test Project 01.pod')
+    with open(path, 'rb') as file:
+        data = file.read()
+    dx = DataExtractor(filepath_or_bytes=data)
+    data = dx.extract_data()
+    print(data.tasks)
---
+++ src/dcma14pointassessor/project_classes.py
@@ -0,0 +1,89 @@
+import datetime as dt
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ProjectBase:
+    def to_dict(self):
+        d = {}
+        for k,v in self.__dict__.items():
+            d[k] = v
+        return d
+
+
+@dataclass(frozen=True)
+class Duration(ProjectBase):
+    value: float
+    unit: str
+
+
+@dataclass(frozen=True)
+class ProjectProperties(ProjectBase):
+    status_date: dt.datetime
+
+
+@dataclass(frozen=True)
+class Task(ProjectBase):
+    id: int
+    name: str
+    baseline_duration: Duration
+    duration: Duration
+    actual_duration: Duration
+    baseline_start: dt.datetime
+    start: dt.datetime
+    actual_start: dt.datetime
+    baseline_finish: dt.datetime
+    finish: dt.datetime
+    actual_finish: dt.datetime
+    baseline_work: Duration
+    work: Duration
+    actual_work: Duration
+    milestone: bool
+    summary: bool
+    critical: bool
+    guid: str
+    resume: dt.datetime
+    stop: dt.datetime
+    constraint_date: dt.datetime
+    constraint_type: str
+    active: bool
+    task_mode: str
+    type: str
+    wbs: str
+    outline_level: int
+    outline_number: str
+
+
+@dataclass(frozen=True)
+class TaskRelation(ProjectBase):
+    lag: Duration
+    notes: str
+    predecessor: str
+    successor: str
+    type: str
+    uid: int
+
+
+@dataclass(frozen=True)
+class ResourceAssignment(ProjectBase):
+    actual_finish: dt.datetime
+    actual_start: dt.datetime
+    actual_work: Duration
+    baseline_finish: dt.datetime
+    baseline_start: dt.datetime
+    baseline_work: Duration
+    create_date: dt.datetime
+    delay: Duration
+    finish: dt.datetime
+    guid: str
+    resource: str
+    start: dt.datetime
+    task_guid: str
+    work: Duration
+
+
+@dataclass(frozen=True)
+class ProjectData:
+    project_properties: ProjectProperties
+    tasks:list[Task]
+    task_relations: list[TaskRelation]
+    resource_assigments: list[ResourceAssignment]
